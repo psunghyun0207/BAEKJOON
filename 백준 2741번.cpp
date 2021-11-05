ff661df366042022dab56da37eac3cbcf5ea9f4a
psunghyun0207 <psunghyun0207@gmail.com>
2021-11-06 00:26:48 +0900

    2021.11.06

---
+++ 백준 2741번.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+
+        printf("%d\n", i);
+    }
+}
