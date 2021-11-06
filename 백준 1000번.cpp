a89c751cc6a291cc77655f8d933a6bb4633ca01a
psunghyun0207 <psunghyun0207@gmail.com>
2021-11-06 23:53:03 +0900

    2020.11.06

---
+++ 백준 1000번.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main() {
+
+    int A, B;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    printf("%d", (A + B));
+}
--- 백준 1001번.cpp
+++ 백준 1001번.cpp
@@ -1,13 +1,9 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
 
 int main() {
-    
-    int A, B;
-    
-    scanf("%d", &A);
-    scanf("%d", &B);
-    
-    printf("%d", (A + B));
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", a - b);
 }
