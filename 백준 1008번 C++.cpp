d070b842ab194ae57f2a88614a6d23938fd8af92
psunghyun0207 <psunghyun0207@gmail.com>
2022-01-16 22:21:41 +0900

    2022.1

---
+++ 백준 1008번 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	double a, b;
+	cin >> a;
+	cin >> b;
+	cout.precision(10);
+
+	cout << a / b << endl;
+}
---
+++ 백준 1330번 C++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, b;
+	cin >> a;
+	cin >> b;
+
+	if (a > b) {
+
+		cout << ">" << endl;
+	}
+	else if (a < b) {
+
+		cout << "<" << endl;
+	}
+	else {
+
+		cout << "==" << endl;
+	}
+}
